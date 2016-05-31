653fa666517ad70edd9038dcd3cafd5ef98e9774
Jason Goebel <goebel.jason@gmail.com>
2016-05-30 19:38:39 -0500

    Added a whole lot

--- js/smooth_page_scroll.js
+++ js/smooth_page_scroll.js
@@ -1,4 +1,53 @@
 $(document).ready(function () {
+
+  var home_pos = $("#home").offset().top;
+  var about_pos = $("#about").offset().top;
+  var portfolio_pos = $("#portfolio").offset().top;
+  var contact_pos = $("#contact").offset().top;
+
+  $(".home-link").addClass("red");
+  $(".home-link").css("fill", "red");
+
+
+  $(window).scroll(function(){
+    var y = $(window).scrollTop();
+
+    /* Not the most efficient way but i'm learning */
+    if (y >= home_pos && y < about_pos){
+      $(".home-link").addClass( "red" );
+      $(".home-link").css("fill", "red");;
+    } else {
+      $(".home-link").removeClass( "red" );
+      $(".home-link").css("fill", "white");
+    };
+
+    if (y >= about_pos && y < portfolio_pos){
+      $(".about-link").addClass( "red" );
+      $(".about-link").css("fill", "red");;
+    } else {
+      $(".about-link").removeClass( "red" );
+      $(".about-link").css("fill", "white");
+    };
+
+    if (y >= portfolio_pos && y < contact_pos){
+      $(".portfolio-link").addClass( "red" );
+      $(".portfolio-link").css("fill", "red");;
+    } else {
+      $(".portfolio-link").removeClass( "red" );
+      $(".portfolio-link").css("fill", "white");
+    };
+
+     if (y >= contact_pos){
+      $(".contact-link").addClass( "red" );
+      $(".contact-link").css("fill", "red");;
+    } else {
+      $(".contact-link").removeClass( "red" );
+      $(".contact-link").css("fill", "white");
+    };
+
+  });
+
+
   // Add smooth scrolling to all links
   $("a").on('click', function (event) {
     // Make sure this.hash has a value before overriding default behavior
@@ -7,7 +56,7 @@ $(document).ready(function () {
       event.preventDefault();
 
       // Store hash
-      var hash = this.hash;
+      var hash = $(this).attr("href");
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
@@ -21,4 +70,5 @@ $(document).ready(function () {
   });
 });
 
+// Slightly modified to accept input from SVG image
 // Thank you www.w3schools.com for this sweet script!!!
